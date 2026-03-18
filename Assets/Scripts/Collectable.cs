c638e1527f5bed3ea72a8c0425e02eead1981a02
ChemsLGDesu <anderson.estrada@tecsup.edu.pe>
2026-03-18 16:46:44 -0500

    Ejercicio 2

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    [SerializeField] private int value = 1;
+
+    [SerializeField] private float timeToDespawn;
+
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().score += value;
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Player : MonoBehaviour
+
+{    public int score = 0;
+    [SerializeField] private float speed = 5.0f;
+
+    public InputSystem_Actions inputs;
+    [SerializeField] private Vector2 MoveInput;
+
+    private void Awake()
+    {
+        inputs = new();
+    }
+
+    private void OnEnable()
+    {
+        inputs.Enable();
+        inputs.Player.Move.performed += OnMovement;
+        inputs.Player.Move.canceled += OnMovement;
+    }
+
+    private void OnMovement(InputAction.CallbackContext context)
+    {        
+        MoveInput = context.ReadValue<Vector2>();
+    }
+
+    private void MovementMechanise(Vector2 input)
+    {
+        transform.position += (Vector3)input * speed * Time.deltaTime;
+    }
+
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (MoveInput != Vector2.zero)
+        {
+            MovementMechanise(MoveInput);
+        }
+    }
+
+
+}
